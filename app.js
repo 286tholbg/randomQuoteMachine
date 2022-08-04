e634190c52e68058a0bc8cefb090b2ddd7a00411
286tholbg <107855431+286tholbg@users.noreply.github.com>
2022-08-04 14:15:27 +1000

    added app.js

---
+++ app.js
@@ -0,0 +1,47 @@
+
+
+window.onload = init;
+function init () {
+    console.log("JS Loaded");
+}
+
+const quotesObj = [
+    {
+        author: "Mark Twain", 
+        quote: "The secret of getting ahead is getting started"
+    }, 
+    {
+        author: "Tony Robbins",
+        quote: "The only impossible journey is the one you never begin"
+    }, 
+    {
+        author: "Walt Disney",
+        quote: "The best way to get started is to quit talking and begin doing"
+    }, 
+    {
+        author: "Jim Rohn",
+        quote: "When you know what you want, and want it bad enough, you’ll find a way to get it"
+    }, 
+    {
+        author: "Henry David Thoreau",
+        quote: "Go confidently in the direction of your dreams. Live the life you have imagined"
+    }, 
+    {
+        author: "Steven Maraboli",
+        quote: "Take action. An inch of movement will bring you closer to your goals than a mile of intention"
+    }, 
+    {
+        author: "Johann Wolfgang von Goethe",
+        quote: "Knowing is not enough; we must apply. Wishing is not enough; we must do"
+    },
+    {
+        author: "Zig Ziglar",
+        quote: "You don’t have to be great to start, but you have to start to be great"
+    },
+]
+
+function generateQuote(){
+    console.log("quote generated");
+    document.getElementById("text").innerText = "Gatluak Tholbok"
+    document.getElementById("author").innerText = "Gat Tholbok"
+}
